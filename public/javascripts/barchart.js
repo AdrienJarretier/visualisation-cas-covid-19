f82f0a02375bbbb38dbde6be3bb99abf8e215955
Oceane Cassan <oceane.cssn@gmail.com>
2020-03-28 00:32:21 +0100

    visu des vraies donnees

--- public/javascripts/barchart.js
+++ public/javascripts/barchart.js
@@ -3,8 +3,8 @@
 $(function (){
 // set the dimensions of the canvas
 var margin = {top: 20, right: 80, bottom: 70, left: 40},
-    width = 600 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+    width = 1200 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
 
 // set the ranges
