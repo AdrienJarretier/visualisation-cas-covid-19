d8f866906036bed815140545014b9a878c664ce4
hugodecasta <hugodecasta@orange.fr>
2020-03-27 23:26:41 +0100

    added accumulation system + separate transform meths

--- db/dataManager.js
+++ db/dataManager.js
@@ -7,57 +7,113 @@ let db = new sqlite3.Database(common.serverConfig.db.database);
 
 // ------------------------------------------------------ INNER DB METHODS
 
-async function select_db(sql, transfer_key=null, remove_transferd_key=false) {
 
+// ------------------ TRANSFORMERS METHODS
+
+function remove_props_inplace(obj, props_to_remove) {
+    for(let id in obj) {
+        for(let prop of props_to_remove) {
+            delete obj[id][prop]
+        }
+    }
+}
+
+function transfer_to_key(rows, transfer_key) {
+
+    let data_obj = {}
+
+    for(let row of rows) {
+        let value_to_transfer = row[transfer_key];
+        data_obj[value_to_transfer] = row;
+    }
+
+    return data_obj
+}
+
+// ------------------ SQL DB CALLER
+
+async function select_db(sql) {
     return new Promise(resolve => {
         db.all(sql, function(err, rows) {
-
-            // -- simple call
-            if (transfer_key == null) {
-                resolve(rows)
-                return
-            }
-
-            // -- transfered call
-            let data_obj = {}
-            for(let row of rows) {
-                let value_to_transfer = row[transfer_key]
-                if(remove_transferd_key) {
-                    delete row[transfer_key]
-                }
-                data_obj[value_to_transfer] = row
-            }
-            resolve(data_obj)
-            return
+            resolve(rows);
         })
     })
 }
 
-async function db_get_all_by_prop(table, prop, value, transfer_key=null, remove_transferd_key=false) {
+// ------------------ SQL SUGAR
+
+async function db_get_all_by_prop(table, prop, value) {
     if (typeof value == 'string') {
-        value = '"' + value + '"'
+        value = '"' + value + '"';
     }
     let sql = 'SELECT * FROM ' + table + ' WHERE ' + prop + ' = ' + value;
-    return await select_db(sql,  transfer_key, remove_transferd_key);
+    return await select_db(sql);
 }
 
-async function db_get_all(table, transfer_key=null, remove_transferd_key=false) {
-    let sql = 'SELECT * FROM ' + table
-    return await select_db(sql,  transfer_key, remove_transferd_key)
+async function db_get_all(table) {
+    let sql = 'SELECT * FROM ' + table;
+    return await select_db(sql);
 }
 
-// ------------------------------------------------------ CORE DATA SYS
+// ------------------------------------------------------ CORE METHODS
+
+
+// ------------------ PROP ACCUMULATION
+
+function compute_data_accumulation(end_date, props, full_rows) {
+
+    return full_rows.reduce((accu, day_data) => {
+
+        let current_date = new Date(day_data.date)
+        if(current_date <= end_date) {
+            for(let prop of props) {
+                accu += day_data[prop]
+            }
+            return accu
+        }
+        return accu
+
+    }, 0)
+}
+
+// ------------------------------------------------------ RETRIEAVAL METHODS
 
 // ------------------ CASE DATA BY GEOID
 
 async function get_data_by_geoid(geoid) {
-    return await db_get_all_by_prop('cases', 'Country', geoid)
+
+    let geodata = await db_get_all_by_prop('cases', 'Country', geoid);
+
+    // -- compute accumulation
+    geodata = geodata.map(day_data => {
+
+        let day_date = new Date(day_data.date)
+
+        day_data.cumul_cases = compute_data_accumulation(day_date, ['cases'], geodata)
+        day_data.cumul_deaths = compute_data_accumulation(day_date, ['deaths'], geodata)
+        day_data.total = day_data.cumul_cases + day_data.cumul_deaths
+        
+        return day_data
+
+    })
+
+    let final_data = transfer_to_key(geodata, 'date');
+    let props_to_remove = ['date', 'country'];
+    remove_props_inplace(final_data, props_to_remove);
+
+    return final_data;
 }
 
 // ------------------ GET ALL COUNTRIES
 
 async function get_all_countries() {
-    return await db_get_all('countries', 'geoid', true)
+
+    let countries = await db_get_all('countries');
+    let final_data = transfer_to_key(countries, 'geoid');
+    let props_to_remove = ['geoid'];
+    remove_props_inplace(final_data, props_to_remove);
+
+    return final_data;
 }
 
 // ------------------------------------------------------ EXPORTS
