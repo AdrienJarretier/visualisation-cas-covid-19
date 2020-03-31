938fd2c40b1e6ee1cb3bb54ca533af4eb73e3792
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2020-03-31 02:05:21 +0200

    - popper v1 for bootstrap dependency - still using popper v2 for tooltip when hovering over bars - changed website title - added a dropdown to later use as a country selector

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { globalTitle: 'Visualisation des cas de Covid-19' });
+router.get('/', function (req, res, next) {
+  res.render('index', { globalTitle: 'Coronavirus - Nouveaux cas chaque jour' });
 });
 
 module.exports = router;
