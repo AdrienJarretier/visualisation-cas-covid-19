1f0c57b97a4f9955aaad90bed166aacd9286d190
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-08-14 11:12:32 +0200

    fixed dropdown with bootstrap v5

--- public/javascripts/getData.js
+++ public/javascripts/getData.js
@@ -1,35 +1,45 @@
 import { get } from './utils.js';
 
-// function disp_countries(countries) {
+function getCases(geoid) {
 
-//     let countriesDropsown = $('#dropdown-items');
+    return get('api/cases/' + geoid);
+}
 
-//     for(let geoid in countries) {
+async function select_country(geoid, name) {
+    // drawBarchart(geoid);
+    $('#countriesDropdown button').text(name);
+}
 
-//         let c_data = countries[geoid]
-//         let name = c_data.name
+function disp_countries(countries) {
 
-//         countriesDropsown.append('a')
-//         .attr('href','#')
-//         .attr('class','dropdown-item')
-//         .html(name)
-//         .on('click', function() {
-//             select_country(geoid, name)
-//         })
-//     }
-// }
+    let countriesDropsown = $('#countriesDropdown ul');
+
+    for (let geoid in countries) {
+
+        let c_data = countries[geoid]
+        let name = c_data.name
+
+        let dropdownItem = $('<li>')
+            .append(
+                $('<a class="dropdown-item" href="#">')
+                    .text(name)
+                    .click(function () {
+                        select_country(geoid, name)
+                    })
+            );
+
+        countriesDropsown.append(dropdownItem);
+    }
+}
 
 async function load_countries() {
 
     let countries = await get("api/countries");
 
-    // disp_countries(countries);
-
-    return countries;
-
-    // let first_geoid = Object.keys(countries)[0]
-    // let first_name = countries[first_geoid].name
-    // select_country(first_geoid, first_name)
+    disp_countries(countries);
+    let first_geoid = Object.keys(countries)[0]
+    let first_name = countries[first_geoid].name
+    select_country(first_geoid, first_name)
 }
 
 export { load_countries };
